bbf8a80b2bea71266a0c166ee2752e328b34286b
pchokanson <pchokanson@9f10d2fa-61e6-11de-a840-2761e47b88d0>
2009-06-28 20:19:12 +0000

    Forgot to add types.h to the repository.  Apologies.

---
+++ trunk/terras/types.h
@@ -0,0 +1,12 @@
+
+
+/** Orientation for the faces of a cube:
+ * North, South, East, West, Up, Down.  Deprecated.
+ */
+enum orient_t {N=0,E,S,W,U,D};
+
+class TerraController;
+class TerraModel;
+class TerraView;
+class TerraNode;
+
