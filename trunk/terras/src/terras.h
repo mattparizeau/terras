b347233e6468f6da0e722f63aea2084e315b24f4
pchokanson <pchokanson@9f10d2fa-61e6-11de-a840-2761e47b88d0>
2009-07-15 23:53:56 +0000

    Adding data directory (with cursors from Pyzzle).  Also moving source to src/ and switching to CMake.

---
+++ trunk/terras/src/terras.h
@@ -0,0 +1,11 @@
+/** @file terras.h
+ * Data structures and prototypes used throughout the Terras engine.
+ * @author Peter Hokanson [pchokanson@gmail.com]
+ */
+
+#ifndef __TERRAS_H__
+#define __TERRAS_H__
+#else
+
+
+#endif
