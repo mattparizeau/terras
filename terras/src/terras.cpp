8e784492d7c4f4d5e203e393b8b39ba8b536a7eb
pchokanson <pchokanson@9f10d2fa-61e6-11de-a840-2761e47b88d0>
2009-07-15 23:53:56 +0000

    Adding data directory (with cursors from Pyzzle).  Also moving source to src/ and switching to CMake.

---
+++ terras/src/terras.cpp
@@ -0,0 +1,12 @@
+/** @file terras.cpp
+ * Main function and other important initialization functions.
+ * @author Peter Hokanson [pchokanson@gmail.com]
+ */
+
+#include "terras.h"
+
+/** Entry point for terras. */
+int main(int argc, char **argv){
+	return 0;
+}
+
