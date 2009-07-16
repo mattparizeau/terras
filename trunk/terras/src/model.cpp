75ab05c78ede27ec56ac8210c2a58b049b0d4377
pchokanson <pchokanson@9f10d2fa-61e6-11de-a840-2761e47b88d0>
2009-07-16 04:24:41 +0000

    Correctly loads Python script now

--- trunk/terras/src/model.cpp
+++ trunk/terras/src/model.cpp
@@ -10,15 +10,18 @@ Model::Model(char *argv0, char *fileName){
 	FILE *f = fopen(fileName,"r");
 	assert(f);
 
+	/* Initialize global and local dictionaries */
+	globals = PyDict_New();
+	locals = PyDict_New();
+
 	Py_SetProgramName(argv0);
 	Py_Initialize();
 
-	
-	PyRun_SimpleString("from time import time,ctime\n"
-                     "print 'Today is',ctime(time())\n");
-//PyRun_FileExFlags(f, fileName, 0, &globals, &locals, 1, 0);
+//	PyRun_SimpleString("from time import time,ctime\n"
+//                     "print 'Today is',ctime(time())\n");
+	PyRun_File(f, fileName, Py_file_input, globals, locals);
 }
 
 Model::~Model(){
-
+	Py_Finalize();
 }
